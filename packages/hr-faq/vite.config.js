c18c064b99ea3c371d676c7c7bdfa540521b3168
tomieric <tomieric@gmail.com>
2023-11-20 22:49:27 +0800

    feat(hr-faq): update build config

--- packages/hr-faq/vite.config.js
+++ packages/hr-faq/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+import pkg from './package.json' assert { type: 'json' }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? `/${pkg.name}/` : `/`,
+    plugins: [react()],
+    build:{
+      outDir: resolve(`../../dist/${pkg.name}`)
+    }
+  }
 })
