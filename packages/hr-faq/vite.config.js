8eef2f07c7f004c4c2b99432dd6540e6d1300aea
tomieric <tomieric@gmail.com>
2023-11-21 17:58:40 +0800

    chore(ci): update url resolve

--- packages/hr-faq/vite.config.js
+++ packages/hr-faq/vite.config.js
@@ -1,13 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
 import pkg from './package.json' assert { type: 'json' }
+
+const resolve = src => fileURLToPath(new URL(src, import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
     base: command === 'build' ? `/${pkg.name}/` : `/`,
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolve('./src')
+      }
+    },
     define: {
       APP_NAME: JSON.stringify(pkg.name),
     },
