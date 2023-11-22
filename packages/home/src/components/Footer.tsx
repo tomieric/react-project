8bfebfff87c421705cfba16a476eaec7bf4e726d
tomieric <tomieric@gmail.com>
2023-11-22 21:57:11 +0800

    feat: add template from

--- packages/home/src/components/Footer.tsx
+++ packages/home/src/components/Footer.tsx
@@ -2,8 +2,8 @@ function Footer() {
   return(
     <footer className="bg-gray-200">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-        <a href="//github.com/tomieric" className="text-xl font-bold text-gray-500 hover:text-gray-400">@tomieric</a>
-        <p className="py-2 text-gray-500 sm:py-0">All rights reserved</p>
+        <a href="//github.com/tomieric" className="text-xl font-bold text-gray-500 hover:text-blue-500 no-underline">@tomieric</a>
+        <p className="py-2 text-gray-500 sm:py-0">All rights reserved, template by <i className="i-carbon-badge inline-flex align-middle text-blue-500"></i> <a href="https://tailwindcomponents.com/" className="text-blue-500">tailwindcomponents.com</a></p>
       </div>
     </footer>
   )
